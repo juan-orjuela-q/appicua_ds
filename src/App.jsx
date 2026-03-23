fc232952d290cdb51071e6b50f6795636dce48d7
Juan Orjuela <juan@appicua.com>
2026-03-23 15:49:37 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Appicua Design System</h1>
+      <p>Welcome to your Design System</p>
+    </div>
+  )
+}
+
+export default App
